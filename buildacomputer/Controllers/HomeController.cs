bbde420226b943b9c0de1b0107db5a3eab546cb4
boviguerre <jonathan.guerrette@gmail.com>
2015-08-31 16:44:30 -0400

    Writing in HomeController to create new views

--- buildacomputer/Controllers/HomeController.cs
+++ buildacomputer/Controllers/HomeController.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using buildacomputer.Models;
 
 namespace buildacomputer.Controllers
 {
     public class HomeController : Controller
     {
+        private static PartsAndUsersContext db = new PartsAndUsersContext();
+        private static List<Build> recentBuilds = new List<Build>();
+        
         public ActionResult Index()
         {
+            //Get top 5 entries, ordered by DateTime
+            
+            //recentBuilds = db.builds.Select(m => m.build )
+            
             return View();
         }
 
