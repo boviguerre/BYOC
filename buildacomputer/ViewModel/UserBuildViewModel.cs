ae610ec42cd0fe110b8b4a42cc29bdec5c053886
boviguerre <jonathan.guerrette@gmail.com>
2015-09-08 23:06:43 -0400

    Added UserBuild viewmodel to save new builds to the database

---
+++ buildacomputer/ViewModel/UserBuildViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using buildacomputer.Models;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace buildacomputer.ViewModel
+{
+    public class UserBuildViewModel
+    {
+        [Display(Name = "Enter a name for your build: ")]
+        [Required]
+        public string buildName { get; set; }
+
+        [Display(Name = "What type of build is this? ")]
+        [Required]
+        public string buildType { get; set; }
+
+        public DateTime buildTime { get; set; }
+    }
+}
